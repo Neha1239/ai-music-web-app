e73b21a4328f4d8df04c55d1d50526bba430e941
Neha1239 <86868752+Neha1239@users.noreply.github.com>
2022-06-07 17:19:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+rightWristx="";
+rightWristy="";
+leftWristx="";
+leftWristy=""; 
 song1="";
 song2="";
 function preload() {
@@ -9,7 +13,25 @@ function setup() {
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 function draw() {
     image(video,0,0,600,500);
+}
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+         
+        leftWristx = results[0].pose.leftWrist.x;
+        leftWristy = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
+
+        rightWristx = results[0].pose.rightWrist.x;
+        rightWristy = results[0].pose.rightWrist.y;
+        console.log("rightWristx = "+rightWristx+" rightWristy = "+rightWristy);
+    }
 }
